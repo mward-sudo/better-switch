5affc43db4b1c6c283841aca939ee59bae8a98dc
Michael Ward <mward82@gmail.com>
2022-03-20 23:17:25 +0000

    Apply linting to source files Fixes #77

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,7 @@ export type CaseObject<T> = Record<string, CaseObjectFunction<T>>
 export type CaseObjectFunction<T> = () => T
 // BetterSwitchTest and BetterSwitchError both reuse the parameters from BetterSwitch
 export type BetterSwitchTest = (...args: Parameters<BetterSwitch>) => boolean
-export type BetterSwitchError = (error: unknown, ...args: Parameters<BetterSwitch>) => string
+export type BetterSwitchError = (
+  error: unknown,
+  ...args: Parameters<BetterSwitch>
+) => string
